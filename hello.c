b9288228bd1a0f995bf5aeb0e480ecc1f4923a1b
rinij <rinimani7@gmail.com>
2025-04-01 15:40:57 -0600

    more notes added

--- hello.c
+++ hello.c
@@ -11,3 +11,4 @@ int main(void)
 // 1. code hello.c (creates a file called hello.c)
 // 2. make hello (identify the file and initiates the compiling of the code)
 // 3 ./hello (run the program called hello)
+//note that hello is the file that the compiler has created after compiling the source code with the ./hello command
