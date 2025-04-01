8effad0a3eee299eb8c10d554b277734e613c7f7
rinij <rinimani7@gmail.com>
2025-04-01 15:37:57 -0600

    created first c programming code

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+    printf("hello, world\n");
+}
+
+
+// There are 3 steps in executing a code in c:
+// 1. code hello.c (creates a file called hello.c)
+// 2. make hello (identify the file and initiates the compiling of the code)
+// 3 ./hello (run the program called hello)
